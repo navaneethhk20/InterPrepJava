33a257ff24382ba702127451a747c1335878ff4a
navaneethhhk <hknavaneeth@gmail.com>
2025-06-29 15:59:49 +0530

    2906

---
+++ src/Ex29062025/Part2/Lab04.java
@@ -0,0 +1,13 @@
+package Ex29062025.Part2;
+
+import java.util.Scanner;
+
+public class Lab04 {
+    public static void main(String[] args) {
+        //To find even or odd
+        int a=989898;
+//        int b=2;
+      String result = (a%2==0)?"even":"odd";
+        System.out.println(result);
+    }
+}
