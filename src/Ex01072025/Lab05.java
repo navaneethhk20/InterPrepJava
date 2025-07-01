9a43e11576389bfb602b832d3acc4c1c096c7461
navaneethhhk <hknavaneeth@gmail.com>
2025-07-01 07:55:11 +0530

    0107

---
+++ src/Ex01072025/Lab05.java
@@ -0,0 +1,16 @@
+package Ex01072025;
+
+public class Lab05 {
+    public static void main(String[] args) {
+        //palindrome of a string
+        String name = "Madam";
+        StringBuilder sb= new StringBuilder(name);
+       String rev_name= sb.reverse().toString();
+        System.out.println(rev_name);
+       if(name.equalsIgnoreCase(rev_name)){
+           System.out.println("it is a palindrome");
+       }else {
+           System.out.println("not a palindrome");
+       }
+    }
+}
