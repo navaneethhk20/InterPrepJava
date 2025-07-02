4430fe4323209b77b87cb19ae545cedea069711c
navaneethhhk <hknavaneeth@gmail.com>
2025-07-02 19:22:20 +0530

    0207

---
+++ src/Ex02072025/Part2/Lab07.java
@@ -0,0 +1,14 @@
+package Ex02072025.Part2;
+
+public class Lab07 {
+    public static void main(String[] args) {
+        //pyramid
+        int n=5;
+        for(int i=1;i<=5;i++){
+            for (int j=1;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
