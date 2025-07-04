3ad25a0ae3b5a4bf283b4cdd150eba5c9e1537a5
navaneethhhk <hknavaneeth@gmail.com>
2025-07-04 07:33:13 +0530

    0407

---
+++ src/Ex04072025/Abstraction/Lab01.java
@@ -0,0 +1,8 @@
+package Ex04072025.Abstraction;
+
+public class Lab01 extends Son {
+    public static void main(String[] args) {
+        Son son = new Son();
+        son.loan50k();
+    }
+}
---
+++ src/Ex04072025/Encapsulation/ICICI_Bank.java
@@ -0,0 +1,32 @@
+package Ex04072025.Encapsulation;
+
+public class ICICI_Bank {
+
+    private String name;
+    private long balance;
+
+    public ICICI_Bank(String name, long balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        if (balance > 0) {
+            this.balance = balance;
+        } else {
+            System.out.println("Not allowed");
+        }
+    }
+}
