200e2836293a746ca9cdd772b9a0e8e9bde1b489
navaneethhhk <hknavaneeth@gmail.com>
2025-07-05 08:12:06 +0530

    0507

---
+++ src/Ex05072025/Strings/OccurenceOfEachCharacter.java
@@ -0,0 +1,25 @@
+package Ex05072025.Strings;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccurenceOfEachCharacter {
+    public static void main(String[] args) {
+        //Occurence Of Each Character
+        String str = "navaneeth";
+
+        HashMap<Character, Integer> charCount = new HashMap<>();
+
+
+        for (char ch : str.toCharArray()) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+        }
+
+
+        System.out.println("Character frequencies:");
+
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            System.out.println(entry.getKey() + " occurs " + entry.getValue() + " times");
+        }
+    }
+}
