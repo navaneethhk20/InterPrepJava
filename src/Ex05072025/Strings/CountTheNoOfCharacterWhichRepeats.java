d82327cdae2fc7f17a59ef53c89adfaf2cb87562
navaneethhhk <hknavaneeth@gmail.com>
2025-07-05 07:23:58 +0530

    0507

---
+++ src/Ex05072025/Strings/CountTheNoOfCharacterWhichRepeats.java
@@ -0,0 +1,8 @@
+package Ex05072025.Strings;
+
+public class CountTheNoOfCharacterWhichRepeats {
+    public static void main(String[] args) {
+        //count the no of character which repeats
+        String str =" Ramesh Sachin Tendulkar";
+    }
+}
