52933200208d0c37779af9ab3ff2aa23fbd2602c
navaneethhhk <hknavaneeth@gmail.com>
2025-07-08 06:47:24 +0530

    0807

---
+++ src/Ex08072025/Arrays/AddTwoArrAndRemoveDuplicate.java
@@ -0,0 +1,10 @@
+package Ex08072025.Arrays;
+
+public class AddTwoArrAndRemoveDuplicate {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 4, 6, 8};
+        int[] arr2 = {3, 4, 5, 7, 9, 8};
+
+
+    }
+}
