72d32fd80d4c4bd28e6114668963255aaa238211
navaneethhhk <hknavaneeth@gmail.com>
2025-07-12 08:03:08 +0530

    1207

---
+++ src/EX12072025/Lab04.java
@@ -0,0 +1,18 @@
+package EX12072025;
+
+import java.util.Scanner;
+
+public class Lab04 {
+    public static void main(String[] args) {
+        //check if a character is vowel or not
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the character");
+        char ch = sc.next().charAt(0);
+        if(ch == 'A' ||ch == 'E' ||ch == 'I' ||ch == 'O' ||ch == 'U' ||ch == 'a' ||ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u'){
+            System.out.println(ch+" character is vowel");
+        }else {
+            System.out.println(ch+ " character is consonant");
+        }
+        sc.close();
+    }
+}
