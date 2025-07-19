a122cd9401d735f31d7a20220759c7bd502c96e3
navaneethhhk <hknavaneeth@gmail.com>
2025-07-19 22:05:10 +0530

    1907

--- src/Ex19072025/Part3/Lab04.java
+++ src/Ex19072025/Part3/Lab04.java
@@ -1,8 +1,21 @@
 package Ex19072025.Part3;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Lab04 {
     public static void main(String[] args) {
         //count the no of characters
         String input = "ttest@!@#123ggg";
+
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for(char ch: input.toCharArray()) {
+            if (Character.isAlphabetic(ch)) {
+                hashMap.put(ch, hashMap.getOrDefault(ch, 0) + 1);
+            }
+        }
+
+        System.out.println(hashMap.size());
+
     }
 }
