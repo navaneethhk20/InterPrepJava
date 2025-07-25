317313ed484a43cb7ba07d3362bee5febf3e758f
navaneethhhk <hknavaneeth@gmail.com>
2025-07-25 18:29:28 +0530

    2507

--- src/Ex252627072025/Part1/Lab16.java
+++ src/Ex252627072025/Part1/Lab16.java
@@ -1,8 +1,30 @@
 package Ex252627072025.Part1;
 
+import java.util.Arrays;
+
 public class Lab16 {
+
+    public static boolean checkAnagram(String st, String st2){
+        st =st.toLowerCase();
+        st2= st2.toLowerCase();
+
+        if(st.length() == st2.length()){
+            return true;
+        }
+
+        char [] arr1 =st.toCharArray();
+        char [] arr2 =st2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        Arrays.equals(arr1,arr2);
+
+        return true;
+    }
     public static void main(String[] args) {
-        // to check anagram
+        String str1 ="silent";
+        String str2 ="listen";
+        System.out.println(checkAnagram(str1,str2));
 
     }
 }
--- src/Ex252627072025/Part1/Lab18.java
+++ src/Ex252627072025/Part1/Lab18.java
@@ -1,7 +1,26 @@
 package Ex252627072025.Part1;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Lab18 {
     public static void main(String[] args) {
         //Remove the repeating & non repeating character
+        String input ="Navaneeth";
+        input=input.toLowerCase();
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+        for(char ch: input.toCharArray()){
+            hashmap.put(ch, hashmap.getOrDefault(ch, 0)+1);
+        }
+
+        HashSet<Character> hashset = new HashSet<>();
+        for(char ch:input.toCharArray()){
+            if(hashmap.get(ch)==1 &&! hashset.contains(ch)){
+                System.out.print(ch+ " ");
+                hashset.add(ch);
+            }
+        }
+
+
     }
 }
