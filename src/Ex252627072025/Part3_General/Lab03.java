c2ac21cdf6f0cc2e8c69814add22248f8c507fdf
navaneethhhk <hknavaneeth@gmail.com>
2025-07-25 21:58:14 +0530

    2507

---
+++ src/Ex252627072025/Part3_General/Lab03.java
@@ -0,0 +1,19 @@
+package Ex252627072025.Part3_General;
+
+import java.util.Scanner;
+
+public class Lab03 {
+    public static void main(String[] args){
+        //Even or odd
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+
+        if(num%2==0){
+            System.out.println(num+" is a even number");
+        }else {
+            System.out.println(num+" is a odd number");
+        }
+        sc.close();
+    }
+}
---
+++ src/Ex252627072025/Part3_General/Lab05.java
@@ -0,0 +1,20 @@
+package Ex252627072025.Part3_General;
+
+import java.util.Scanner;
+
+public class Lab05 {
+    public static void main(String[] args) {
+        //check a character is vowel or consonant
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the character");
+        char ch = sc.next().charAt(0);
+
+        if(ch=='A'|| ch=='E'||ch=='I'|| ch=='O'||ch=='U'|| ch=='a'||ch=='e'|| ch=='i'||ch=='o'|| ch=='u'){
+            System.out.println(ch+" is a vowel");
+        }else {
+            System.out.println(ch+ " is a consonant");
+        }
+        sc.close();
+
+    }
+}
