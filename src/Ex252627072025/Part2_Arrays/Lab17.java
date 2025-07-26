2da8457e40c44cfe3de52aef45c857c51bb63f10
navaneethhhk <hknavaneeth@gmail.com>
2025-07-26 18:57:24 +0530

    2607

---
+++ src/Ex252627072025/Part2_Arrays/Lab17.java
@@ -0,0 +1,17 @@
+package Ex252627072025.Part2_Arrays;
+
+public class Lab17 {
+    public static void main(String[] args) {
+        //output: {120,24,12,8,6}
+        int[] arr = {1, 2, 3, 4,5};
+        int totalProduct = 1;
+
+        for (int num : arr) {
+            totalProduct = totalProduct*num;
+        }
+
+        for (int num : arr) {
+            System.out.print((totalProduct / num)+" ");
+        }
+    }
+}
