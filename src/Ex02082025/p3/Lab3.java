f5108f3f40a81af1bd5210f9deaec531f404ae3a
navaneethhhk <hknavaneeth@gmail.com>
2025-08-02 17:48:21 +0530

    0208

---
+++ src/Ex02082025/p3/Lab3.java
@@ -0,0 +1,25 @@
+package Ex02082025.p3;
+
+public class Lab3 {
+    public static void main(String[] args) {
+        //sort and find second largest &smallest in an array
+        int[] arr={4,5,6,54,54,52,32,63,52,653,65,6,6,65,69,9,68,85,89,9,86,95,7,47,47};
+        int temp=arr[0];
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        System.out.println(arr[arr.length-2]);
+        System.out.println(arr[1]);
+    }
+}
---
+++ src/Ex02082025/p3/Lab4.java
@@ -0,0 +1,20 @@
+package Ex02082025.p3;
+
+import java.util.HashSet;
+
+public class Lab4 {
+    public static void main(String[] args) {
+        //add two array and remove duplicates
+        int[] arr1={4,5,6,8,9,10};
+        int[] arr2={1,3,5,2,7,8,9};
+
+        HashSet<Integer>hashSet = new HashSet<>();
+        for(int num:arr1){
+            hashSet.add(num);
+        }
+        for(int num:arr2){
+            hashSet.add(num);
+        }
+        System.out.println(hashSet.toString());
+    }
+}
