b721cfc59261081cbbb1c975946145506b449eab
navaneethhhk <hknavaneeth@gmail.com>
2025-08-05 08:36:06 +0530

    0508

--- src/Ex05082025/p1/Lab1.java
+++ src/Ex05082025/p1/Lab1.java
@@ -1,4 +1,14 @@
 package Ex05082025.p1;
 
 public class Lab1 {
+    public static void main(String[] args) {
+        //Reverse a string
+        String input ="Navaneeth";
+        String rev="";
+        for(int i=input.length()-1;i>=0;i--){
+            rev=rev+input.charAt(i);
+        }
+        System.out.println(rev);
+    }
+
 }
