3655cf3e851acad7b04c1a23b9c6c1caafc3c155
navaneethhhk <hknavaneeth@gmail.com>
2025-08-06 08:41:29 +0530

    0508

--- src/Ex06082025/P1/Lab7.java
+++ src/Ex06082025/P1/Lab7.java
@@ -1,8 +1,25 @@
 package Ex06082025.P1;
 
+import java.util.HashMap;
+
 public class Lab7 {
     public static void main(String[] args) {
         String input="tomorrow";
+        HashMap<Character,Integer> hashmap = new HashMap<>();
+        for(char ch: input.toCharArray()){
+            hashmap.put(ch, hashmap.getOrDefault(ch,0)+1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<=input.length()-1;i++){
+            char currentchar= input.charAt(i);
+            int frequency=hashmap.get(currentchar);
+            if(frequency==1 || frequency==2){
+                sb.append(currentchar);
+            }else {
+                sb.append("$");
+            }
+        }
+        System.out.println(sb.toString());
 
     }
 }
