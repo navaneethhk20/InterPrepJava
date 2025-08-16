8efc8b080749895e0410a09a884e71d75328435e
navaneethhhk <hknavaneeth@gmail.com>
2025-08-16 14:57:30 +0530

    1608

---
+++ src/Ex16082025/p2/L5.java
@@ -0,0 +1,25 @@
+package Ex16082025.p2;
+
+import java.util.HashMap;
+
+public class L5 {
+    public static void main(String[] args) {
+        String input="TOMORROW";
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch: input.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<=input.length()-1;i++){
+            char currentchar = input.charAt(i);
+            int frequency= map.get(currentchar);
+            if(frequency==1 || frequency==2){
+                sb.append(currentchar);
+            }else {
+                sb.append("$");
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ src/Ex16082025/p2/L9.java
@@ -0,0 +1,8 @@
+package Ex16082025.p2;
+
+public class L9 {
+    public static void main(String[] args) {
+        int[] arr={635,21,32,625,8,54,352,342,615,56,52,5448,98,65,323,241,54,81,65,32,281,75,86,978,4754};
+
+    }
+}
