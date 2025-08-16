0e2292cb95deaa99d4abc306b75e35f2a0d77e00
navaneethhhk <hknavaneeth@gmail.com>
2025-08-16 11:20:29 +0530

    1608

---
+++ src/Ex16082025/p1/p1.java
@@ -0,0 +1,10 @@
+package Ex16082025.p1;
+
+public class p1 {
+    public static void main(String[] args) {
+        String input="Nithu";
+        StringBuilder sb = new StringBuilder(input);
+        String rev= sb.reverse().toString();
+        System.out.println(rev);
+    }
+}
