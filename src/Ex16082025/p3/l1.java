ea0086be439b47331a4d9a013e2f8c10f0926ff2
navaneethhhk <hknavaneeth@gmail.com>
2025-08-16 22:10:56 +0530

    1608

--- src/Ex16082025/p3/l1.java
+++ src/Ex16082025/p3/l1.java
@@ -9,7 +9,7 @@ public class l1 {
         int num3=sc.nextInt();
 
         int max=0;
-        max=(num1>num2)?((num1>num3)?num1:num3)((num2>num3))?num2:num3;
+        max=(num1>num2)?((num1>num3)?num1:num3):((num2>num3))?num2:num3;
         System.out.println(max);
     }
 }
