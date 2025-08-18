03fa2d32ecd9fb142c904bf9ca9d1dd99efafa0b
navaneethhhk <hknavaneeth@gmail.com>
2025-08-18 21:00:03 +0530

    1808

---
+++ src/Ex18082025/p3/l7.java
@@ -0,0 +1,29 @@
+package Ex18082025.p3;
+
+import java.util.Scanner;
+
+public class l7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int num= sc.nextInt();
+        int first=0;
+        int second=1;
+
+        if(num>=1){
+            System.out.print(first+" ");
+        }
+
+        if(num>=2){
+            System.out.print(second+" ");
+        }
+
+        for(int i=3;i<=num;i++){
+            int next= first+second;
+            System.out.print(next+" ");
+            first=second;
+            second=next;
+        }
+
+    }
+}
