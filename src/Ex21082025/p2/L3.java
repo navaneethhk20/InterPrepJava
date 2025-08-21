15e601c8b338484584488a7d5f28d74d6f94400f
navaneethhhk <hknavaneeth@gmail.com>
2025-08-21 09:23:41 +0530

    2108

---
+++ src/Ex21082025/p2/L3.java
@@ -0,0 +1,19 @@
+package Ex21082025.p2;
+
+import java.util.TreeSet;
+
+public class L3 {
+    public static void main(String[] args) {
+        int []arr={4,8,1,23,63,589};
+        int[] arr2={54,8,6,1,84 ,9};
+
+        TreeSet<Integer>list= new TreeSet<>();
+        for(int num: arr){
+            list.add(num);
+        }
+        for(int num: arr2){
+            list.add(num);
+        }
+        System.out.println(list);
+    }
+}
---
+++ src/Ex21082025/p2/L6.java
@@ -0,0 +1,15 @@
+package Ex21082025.p2;
+
+public class L6 {
+    public static void main(String[] args) {
+        int [] arr={654,64,76,86,987,3,243,41,45,67897,654,32,1,65,45,6789,65432,1,444487965,43,21,5,4566675,4};
+
+        for(int i=0;i<=arr.length-1;i++){
+            for(int j=i+1;j<=arr.length-1;j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]+" is present at "+i+" and "+j);
+                }
+            }
+        }
+    }
+}
