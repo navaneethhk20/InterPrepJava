8eb7b8e209c77b5db30359d7ca0bb3f427d77a70
navaneethhhk <hknavaneeth@gmail.com>
2025-08-21 07:57:53 +0530

    2108

---
+++ src/Ex21082025/p1/L15.java
@@ -0,0 +1,13 @@
+package Ex21082025.p1;
+
+public class L15 {
+    public static void main(String[] args) {
+        String input="I Love Java Programming";
+        String [] words= input.split("\\s+");
+        String rev="";
+        for(int i = words.length-1;i>=0;i--){
+            rev=rev+words[i]+" ";
+        }
+        System.out.println(rev);
+    }
+}
