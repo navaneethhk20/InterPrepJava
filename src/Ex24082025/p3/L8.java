6d8f22cac3d576a31b9e320f1977e760eb1c2b29
navaneethhhk <hknavaneeth@gmail.com>
2025-08-24 15:27:09 +0530

    2408

---
+++ src/Ex24082025/p3/L8.java
@@ -0,0 +1,30 @@
+package Ex24082025.p3;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class L8 {
+    public static boolean isAnagram(String st1, String st2){
+        st1= st1.toLowerCase();
+        st2=st2.toLowerCase();
+        if(st1.length()!=st2.length()){
+            return false;
+        }
+
+        char[] arr1= st1.toCharArray();
+        char [] arr2= st2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1,arr2);
+
+    }
+
+
+
+    public static void main(String[] args) {
+        String s1="aas";
+         String s2= "Car";
+         System.out.println(isAnagram(s1,s2));
+    }
+}
