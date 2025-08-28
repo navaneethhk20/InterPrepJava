f4bed2fe4c43c387cb57771eb5d9231f2ebe0f16
navaneethhhk <hknavaneeth@gmail.com>
2025-08-28 22:57:56 +0530

    2808

---
+++ src/EX28082025/p2/L5.java
@@ -0,0 +1,27 @@
+package EX28082025.p2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class L5 {
+    public static boolean isAnagram(String str1, String str2){
+        str1=str1.toLowerCase();
+        str2=str2.toLowerCase();
+
+        if(str1.length()!=str2.length()){
+            return false;
+        }
+
+        char [] arr1= str1.toCharArray();
+        char [] arr2= str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1,arr2);
+    }
+    public static void main(String[] args) {
+        String s1= "silent";
+        String s2= "listen";
+        System.out.println(isAnagram(s1,s2));
+    }
+}
---
+++ src/EX28082025/p2/L9.java
@@ -0,0 +1,7 @@
+package EX28082025.p2;
+
+public class L9 {
+    public static void main(String[] args) {
+
+    }
+}
