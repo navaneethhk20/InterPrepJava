7a1a10d82d767ff6f6e5016ca8cb417b2b1b3f9e
navaneethhhk <hknavaneeth@gmail.com>
2025-08-29 12:52:22 +0530

    2908

---
+++ src/EX28082025/p3/L9.java
@@ -0,0 +1,14 @@
+package EX28082025.p3;
+
+public class L9 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        int product=1;
+        for(int num:arr){
+            product=product*num;
+        }
+        for(int num:arr){
+            System.out.print(product/num+" ");
+        }
+    }
+}
