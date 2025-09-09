08bc05983b47a4ce09a359ceebff725d5daecfef
navaneethhhk <hknavaneeth@gmail.com>
2025-09-09 21:55:26 +0530

    1009

---
+++ src/Ex09092025/i11.java
@@ -0,0 +1,14 @@
+package Ex09092025;
+
+public class i11 {
+    public static void main(String[] args) {
+        int [] arr={1,2,3,4};
+        int product=1;
+        for(int num: arr){
+            product=product*num;
+        }
+        for(int num:arr){
+            System.out.print(product/num+" ");
+        }
+    }
+}
---
+++ src/Ex09092025/l3.java
@@ -0,0 +1,15 @@
+package Ex09092025;
+
+public class l3 {
+    public static void main(String[] args) {
+        String input="Nithu Hosur Kumar";
+        String [] words = input.split("\\s+");
+        String lastword= words[words.length-1];
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<words.length-1;i++){
+            sb.append(words[i].charAt(0)).append(".");
+        }
+        sb.append(" ").append(lastword);
+        System.out.println(sb.toString());
+    }
+}
