221da48b403717b60491ed59acd914e24e30c093
navaneethhhk <hknavaneeth@gmail.com>
2025-09-09 07:34:52 +0530

    0909

--- src/Ex08092025/p1/l3.java
+++ src/Ex08092025/p1/l3.java
@@ -1,8 +1,21 @@
 package Ex08092025.p1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class l3 {
     public static void main(String[] args) {
        String input="navneetha";
-
+       HashMap<Character, Integer> map = new HashMap<>();
+       for(char ch: input.toCharArray()){
+           map.put(ch, map.getOrDefault(ch,0)+1);
+       }
+       ArrayList<Character> list= new ArrayList<>();
+       for(char ch:input.toCharArray()){
+           if(map.get(ch)>1 && !list.contains(ch)){
+               list.add(ch);
+           }
+       }
+System.out.println(list);
     }
 }
