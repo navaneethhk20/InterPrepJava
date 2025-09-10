afc387e20026ec0455ca64d95a0b08b2e69e9fe8
navaneethhhk <hknavaneeth@gmail.com>
2025-09-10 08:33:55 +0530

    1009

---
+++ src/Ex10092025/p1/l2.java
@@ -0,0 +1,21 @@
+package Ex10092025.p1;
+
+import java.util.HashMap;
+
+public class l2 {
+    public static void main(String[] args) {
+        String[] s1={"fgd","Hfd","gtre","terw"};
+        String[] s2={"fsd","fds","fgd","gds","terw"};
+
+        HashMap<String, Integer> hashamp= new HashMap<>();
+        for(String common: s1){
+            hashamp.put(common, hashamp.getOrDefault(common,0)+1);
+        }
+
+        for (String common:s2){
+            if(hashamp.containsKey(common)){
+                System.out.println(common);
+            }
+        }
+    }
+}
