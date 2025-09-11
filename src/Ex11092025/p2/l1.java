b8ef2c2dc47a51be3f7cbb7c68ae1c00466d6c76
navaneethhhk <hknavaneeth@gmail.com>
2025-09-11 21:22:00 +0530

    1109

---
+++ src/Ex11092025/p2/l1.java
@@ -0,0 +1,20 @@
+package Ex11092025.p2;
+
+import java.util.Arrays;
+
+public class l1 {
+    public static void main(String[] args) {
+        int [] arr={3,5,0,9,0,5,0,2,0,2,0};
+        int index=0;
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]!=0){
+                arr[index++]=arr[i];
+            }
+        }
+        while(index<=arr.length-1){
+            arr[index++]=0;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+}
---
+++ src/Ex11092025/p2/l3.java
@@ -0,0 +1,15 @@
+package Ex11092025.p2;
+
+public class l3 {
+    public static void main(String[] args) {
+        String input="Whitefield Nithu Kumar";
+        String[] words = input.split("\\s+");
+        String lastword= words[words.length-1];
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i< words.length-1;i++){
+            sb.append(words[i].charAt(0)).append(".");
+        }
+        sb.append(" ").append(lastword);
+        System.out.println(sb.toString());
+    }
+}
