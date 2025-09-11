b91a3dacac1da864facf94c5ba2c0c2f83e03e25
navaneethhhk <hknavaneeth@gmail.com>
2025-09-11 09:28:22 +0530

    1109

---
+++ src/Ex11092025/p1/l6.java
@@ -0,0 +1,16 @@
+package Ex11092025.p1;
+
+public class l6 {
+    public static void main(String[] args) {
+        String input="tew@#teroi@#ioyrepo@#bdvcnmfd@#";
+        String substring = "@#";
+
+        int index=0;
+        int count=0;
+        while((index=input.indexOf(substring, index))!=-1){
+         count++;
+         index= index+substring.length();
+        }
+        System.out.println(count);
+    }
+}
