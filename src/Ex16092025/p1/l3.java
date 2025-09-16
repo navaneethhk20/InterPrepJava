516e5d37baec9adc5c13774b472cf4758a917be0
navaneethhhk <hknavaneeth@gmail.com>
2025-09-16 19:45:35 +0530

    1609

---
+++ src/Ex16092025/p1/l3.java
@@ -0,0 +1,24 @@
+package Ex16092025.p1;
+
+import java.util.HashMap;
+
+public class l3 {
+    public static void main(String[] args) {
+        String input="Whitefield Kadugodi metro";
+        input= input.toLowerCase();
+        int repeatingcount=0;
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+        for(char ch: input.toCharArray()){
+           if(ch!=' '){
+                hashmap.put(ch, hashmap.getOrDefault(ch,0)+1);
+            }
+       }
+        for(int frequency: hashmap.values()){
+            if(frequency>1){
+                repeatingcount++;
+            }
+
+        }
+        System.out.println(repeatingcount);
+    }
+}
