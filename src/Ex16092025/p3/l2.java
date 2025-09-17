f49249199cc701c3dfb4d208c78048e78d568cd7
navaneethhhk <hknavaneeth@gmail.com>
2025-09-17 07:19:57 +0530

    1709

---
+++ src/Ex16092025/p3/l2.java
@@ -0,0 +1,23 @@
+package Ex16092025.p3;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class l2 {
+    public static void main(String[] args) {
+        String input="swisasa";
+        HashMap<Character,Integer> hashmap = new HashMap<>();
+        for(char ch: input.toCharArray()){
+            hashmap.put(ch, hashmap.getOrDefault(ch,0)+1);
+        }
+
+        ArrayList<Character> list = new ArrayList<>();
+        for(char ch: input.toCharArray()){
+            if(hashmap.get(ch)>1&&!list.contains(ch)){
+                list.add(ch);
+            }
+        }
+        System.out.println(list);
+        System.out.println(list.get(0));
+    }
+}
