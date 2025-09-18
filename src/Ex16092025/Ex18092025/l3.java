71bb0f79a18e369a8d64ef51d9c4e24bfdb072e2
navaneethhhk <hknavaneeth@gmail.com>
2025-09-18 08:28:35 +0530

    1809

---
+++ src/Ex16092025/Ex18092025/l3.java
@@ -0,0 +1,18 @@
+package Ex16092025.Ex18092025;
+
+import java.util.Scanner;
+
+public class l3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the character");
+        char ch= sc.next().charAt(0);
+        ch=Character.toLowerCase(ch);
+
+        if(ch=='a' || ch =='e'||ch=='i' || ch =='o'||ch=='u'){
+            System.out.println(ch+ " is a vowel.");
+        }else {
+            System.out.println(ch+" is a consonant.");
+        }
+    }
+}
