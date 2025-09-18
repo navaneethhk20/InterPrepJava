2eecc8b494a01ee3a13c59eda5eb0f67e97d28da
navaneethhhk <hknavaneeth@gmail.com>
2025-09-18 07:20:55 +0530

    1809

---
+++ src/Ex16092025/p3/l8.java
@@ -0,0 +1,26 @@
+package Ex16092025.p3;
+
+public class l8 {
+    public static void main(String[] args){
+        int[] arr={2,5,4,8,9,6,1};
+        int max=arr[0];
+        int min=arr[0];
+        int index=0;
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                index=i;
+            }
+        }
+        for(int j=0;j<=index;j++){
+            if(arr[j]<min){
+                min=arr[j];
+            }
+        }
+
+        int diff= max-min;
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(diff);
+    }
+}
