49a53d8975b21ffc1ea6ca4271978c4441105714
navaneethhhk <hknavaneeth@gmail.com>
2025-09-21 11:09:30 +0530

    2109

---
+++ src/Ex21092025/p1/l7.java
@@ -0,0 +1,15 @@
+package Ex21092025.p1;
+
+import java.util.Arrays;
+
+public class l7 {
+    public static void main(String[] args) {
+        int [] arr={53,42,654,756,865,978,54,234,231,1,453,254,65,467,568,76,7,8};
+        for(int i=0;i<=arr.length-1;i=i+3){
+         int temp=arr[i];
+            arr[i]=arr[i+2];
+            arr[i+2]=temp;
+         }
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/Ex21092025/p1/l9.java
@@ -0,0 +1,17 @@
+package Ex21092025.p1;
+import java.util.Arrays;
+public class l9 {
+    public static void main(String[] args) {
+        int[] arr={2,0,4,0,5,0,6,0,7,0,8,0,9,0,1};
+        int index=0;
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]!=0){
+                arr[index++]=arr[i];
+            }
+        }
+        while(index<=arr.length-1){
+            arr[index++]=0;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
