9b9e08cce963a5174691a0eb511fe3afda88f71c
navaneethhhk <hknavaneeth@gmail.com>
2025-09-21 10:50:50 +0530

    2109

---
+++ src/Ex21092025/p1/l2.java
@@ -0,0 +1,10 @@
+package Ex21092025.p1;
+
+public class l2 {
+    public static void main(String [] args){
+        String input="Design";
+        StringBuilder sb = new StringBuilder(input);
+        String rev= sb.reverse().toString();
+        System.out.println(rev);
+    }
+}
