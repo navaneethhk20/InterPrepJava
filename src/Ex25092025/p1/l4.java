ae49a3fd41a51d302295dc3969d8736eaf4d9f75
navaneethhhk <hknavaneeth@gmail.com>
2025-09-25 08:30:41 +0530

    2509

---
+++ src/Ex25092025/p1/l4.java
@@ -0,0 +1,17 @@
+package Ex25092025.p1;
+
+import java.util.HashSet;
+
+public class l4 {
+    public static void main(String[] args) {
+        String input="navaneeth";
+        HashSet<Character> set= new HashSet<>();
+        StringBuilder sb= new StringBuilder();
+        for(char ch: input.toCharArray()){
+            if(set.add(ch)){
+                sb.append(ch);
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
