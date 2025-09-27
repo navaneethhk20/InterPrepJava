4e1938b9e8436d6f6fc0631fec20fc6e22a28099
navaneethhhk <hknavaneeth@gmail.com>
2025-09-27 20:07:25 +0530

    2709

---
+++ src/Ex20250927/p4/l2.java
@@ -0,0 +1,20 @@
+package Ex20250927.p4;
+
+import java.util.Scanner;
+
+public class l2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the number");
+        int num= sc.nextInt();
+
+        if(num==0){
+            System.out.println(num+" is zero");
+        }else if(num<0){
+            System.out.println(num+" is negative");
+        }else {
+            System.out.println(num+" is positive.");
+        }
+        sc.close();
+    }
+}
