5a92754a2538cb0b1fd59fb0dcf6abffd86c35be
navaneethhhk <hknavaneeth@gmail.com>
2025-09-27 13:13:06 +0530

    2709

---
+++ src/Ex20250927/p3/L10.java
@@ -0,0 +1,29 @@
+package Ex20250927.p3;
+
+import java.util.Scanner;
+
+public class L10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string");
+        String input= sc.nextLine().toLowerCase();
+
+        int vowel=0;
+        int consonant=0;
+        for(int i=0;i<=input.length()-1;i++) {
+            char ch=input.charAt(i);
+            if(Character.isLetter(ch)) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowel++;
+                } else {
+                    consonant++;
+                }
+            }
+        }
+        System.out.println(vowel);
+        System.out.println(consonant);
+        sc.close();
+
+
+    }
+}
