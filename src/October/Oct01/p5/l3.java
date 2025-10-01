774ea6cdd88b664e65660f6dc29dfd268bdd6e8d
navaneethhhk <hknavaneeth@gmail.com>
2025-10-01 20:58:24 +0530

    0110

---
+++ src/October/Oct01/p5/l3.java
@@ -0,0 +1,32 @@
+package October.Oct01.p5;
+
+import java.util.Scanner;
+
+public class l3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the browser");
+        String browser= sc.next().toLowerCase();
+
+        switch (browser){
+            case "chrome":
+                System.out.println("Starting chrome browser");
+                break;
+            case "edge":
+                System.out.println("Starting edge browser");
+                break;
+            case "firefox":
+                System.out.println("Starting firefox browser");
+                break;
+            case "iexplorer":
+                System.out.println("Starting IE browser");
+                break;
+            case "safari":
+                System.out.println("Starting safari browser");
+                break;
+            default:
+                System.out.println("Do not recognize the browser");
+        }
+        sc.close();
+    }
+}
---
+++ src/October/Oct01/p5/l5.java
@@ -0,0 +1,30 @@
+package October.Oct01.p5;
+
+import java.util.Scanner;
+
+public class l5 {
+    public static boolean isPrime(int number){
+        if(number<=1){
+            return false;
+        }
+        if(number==2){
+            return true;
+        }
+        if(number%2==0){
+            return false;
+        }
+        for(int i=3;i*i<=number;i=i+2){
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num=sc.nextInt();
+
+        System.out.println(isPrime(num));
+    }
+}
