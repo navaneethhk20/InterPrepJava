9da65cbe223cef3dfa5e03fd7eea761beaa266a6
navaneethhhk <hknavaneeth@gmail.com>
2025-10-01 21:37:31 +0530

    0110

--- src/October/Oct01/p1/l1.java
+++ src/October/Oct01/p1/l1.java
@@ -2,9 +2,14 @@ package October.Oct01.p1;
 
 public class l1 {
     public static void main(String[] args){
-        String input="restaurant";
+        String input="madam";
         StringBuilder sb= new StringBuilder(input);
         String rev= sb.reverse().toString();
         System.out.println(rev);
+        if(input.equals(rev)){
+            System.out.println("palindrome");
+        }else {
+            System.out.println("not palindrome");
+        }
     }
 }
