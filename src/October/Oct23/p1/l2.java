8494b6d2ed7e5f382fa6ba0a899acd6457cf5ad6
navaneethhhk <hknavaneeth@gmail.com>
2025-10-23 08:08:22 +0530

    1310

--- src/October/Oct23/p1/l2.java
+++ src/October/Oct23/p1/l2.java
@@ -2,6 +2,11 @@ package October.Oct23.p1;
 
 public class l2 {
     public static void main(String[] args){
-
+      String input="jetbrains";
+      String rev="";
+      for(int i=input.length()-1;i>=0;i--){
+          rev=rev+input.charAt(i);
+      }
+      System.out.println(rev);
     }
 }
