19ab978881dd3d36d1f3726395f64246a51f5365
navaneethhhk <hknavaneeth@gmail.com>
2025-10-23 08:03:28 +0530

    1310

--- src/October/Oct23/p1/l1.java
+++ src/October/Oct23/p1/l1.java
@@ -3,6 +3,9 @@ package October.Oct23.p1;
 public class l1 {
     public static void main(String[] args){
         String input="nithu";
+        StringBuilder sb= new StringBuilder();
+        String rev=sb.reverse().toString();
+        System.out.println(rev);
 
 
     }
