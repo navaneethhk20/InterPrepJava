a4cc1c5dd358a7469088e5d2295d810a6629e9a5
navaneethhhk <hknavaneeth@gmail.com>
2025-10-24 19:41:14 +0530

    2410

---
+++ src/October/oct24/p2/l8.java
@@ -0,0 +1,12 @@
+package October.oct24.p2;
+
+public class l8 {
+    public static void main(String[] args) {
+        String input="I Love Java Prograaming";
+        String[] words= input.trim().split("\\s+");
+        String lastword= words[words.length-1];
+        StringBuilder sb= new StringBuilder(lastword);
+        words[words.length-1]=sb.reverse().toString();
+        System.out.println(String.join(" ", words));
+    }
+}
