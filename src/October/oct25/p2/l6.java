b905c056f58c14430ea0ee40d1eeca7345b01eaa
navaneethhhk <hknavaneeth@gmail.com>
2025-10-25 13:40:23 +0530

    2510

---
+++ src/October/oct25/p2/l6.java
@@ -0,0 +1,17 @@
+package October.oct25.p2;
+
+import java.util.Arrays;
+
+public class l6 {
+    public static void main(String[] args) {
+        int[] arr={54,8,26,65,98,54,9,21,4};
+        for(int i=0;i<=arr.length-1;i=i+3){
+
+                int temp=arr[i];
+                arr[i]=arr[i+2];
+                arr[i+2]=temp;
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ src/October/oct25/p2/l9.java
@@ -0,0 +1,19 @@
+package October.oct25.p2;
+
+import java.util.Arrays;
+
+public class l9 {
+    public static void main(String[] args) {
+        int[] arr={2,30,5,20,6,10,7,50,8,70,1};
+        for(int i=0;i<=arr.length-1;i++){
+            for(int j=i+1;j<=arr.length-1;j++){
+                if(arr[i]>arr[j]){
+                    int tem=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=tem;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
