95bf63079000006a7d813fc1e563c30c7294c8f5
navaneethhhk <hknavaneeth@gmail.com>
2025-10-26 12:32:31 +0530

    2610

---
+++ src/October/oct25/p4/l7.java
@@ -0,0 +1,26 @@
+package October.oct25.p4;
+
+import java.util.Scanner;
+
+public class l7 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the browser");
+        String browser= sc.next().toLowerCase();
+
+        switch(browser){
+            case "chrome":
+                System.out.println("starting chrome");
+                break;
+            case "edge":
+                System.out.println("starting edge");
+                break;
+            case  "firefox":
+                System.out.println("firefox");
+                break;
+            default:
+                System.out.println("specify correct browser");
+        }
+        sc.close();
+    }
+}
---
+++ src/October/oct25/p4/l9.java
@@ -0,0 +1,31 @@
+package October.oct25.p4;
+
+import java.util.Scanner;
+
+public class l9 {
+    public static  Boolean isPrime(int number){
+        if(number<=1){
+            return false;
+        }
+        if(number==2){
+            return true;
+        }
+        if(number%2==0){
+            return false;
+        }
+
+        for(int i=3;i*i<=number;i++){
+            if(number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        for(int i=0;i<=100;i++){
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
+}
