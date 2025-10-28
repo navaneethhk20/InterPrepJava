d2d51e9bf1ee648485d59023d3611d714e105f59
navaneethhhk <hknavaneeth@gmail.com>
2025-10-28 07:59:32 +0530

    2610

--- src/October/Oct01/p4/l9.java
+++ src/October/Oct01/p4/l9.java
@@ -6,21 +6,18 @@ public class l9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 3 sides");
-        int side1=sc.nextInt();
-        int side2=sc.nextInt();
-        int side3=sc.nextInt();
+        int side1 = sc.nextInt();
+        int side2 = sc.nextInt();
+        int side3 = sc.nextInt();
 
-        if(side1==side2 && side1==side3 && side2==side3){
+        if (side1 == side2 && side1 == side3 && side2 == side3) {
             System.out.println("Equilateral triangle");
-        } else if ((side1==side2)||(side2==side3)||(side1==side3)) {
+        } else if ((side1 == side2) || (side2 == side3) || (side1 == side3)) {
             System.out.println("Isoscelus");
-        }else {
+        } else {
             System.out.println("Scalene");
         }
         sc.close();
     }
-
-
-
 
 }
