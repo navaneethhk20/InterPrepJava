c4d5e475f662a4cfec23097e07e10f95d667de74
navaneethhhk <hknavaneeth@gmail.com>
2025-10-29 21:16:38 +0530

    2910

---
+++ src/October/oct29/p1/l5.java
@@ -0,0 +1,17 @@
+package October.oct29.p1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class l5 {
+    public static void main(String[] args) {
+        String input="Gdfgregfdsf";
+        HashMap<Character,Integer> hashmap = new HashMap<>();
+        for(char ch: input.toCharArray()){
+            hashmap.put(ch, hashmap.getOrDefault(ch,0)+1);
+        }
+        for(Map.Entry<Character,Integer> entry: hashmap.entrySet()){
+            System.out.println(entry.getKey()+" occurs "+entry.getValue()+" times.");
+        }
+    }
+}
---
+++ src/October/oct29/p1/l8.java
@@ -0,0 +1,19 @@
+package October.oct29.p1;
+
+import java.util.Arrays;
+
+public class l8 {
+    public static void main(String[] args) {
+        int [] arr={2,0,5,0,9,0,8,0,1,0,4};
+        int index=0;
+        for(int i=0;i<=arr.length-1;i++){
+            if(arr[i]!=0){
+                arr[index++]=arr[i];
+            }
+        }
+        while(index<=arr.length-1){
+            arr[index++]=0;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
