6b0483038bc580d8e78c3fef6905854e4325ee27
navaneethhhk <hknavaneeth@gmail.com>
2025-11-03 19:00:36 +0530

    3010

--- src/November/p2/l1.java
+++ src/November/p2/l1.java
@@ -1,4 +1,10 @@
 package November.p2;
 
 public class l1 {
+    public static void main(String[] args) {
+    String input="nithu";
+    StringBuilder sb = new StringBuilder(input);
+    String rev= sb.reverse().toString();
+    System.out.println(rev);
+    }
 }
---
+++ src/November/p2/l4.java
@@ -0,0 +1,26 @@
+package November.p2;
+
+import java.util.HashMap;
+
+public class l4 {
+    public static void main(String[] args) {
+        String input="Ramesh Sachin Tendulkar";
+        input=input.toLowerCase();
+        int repeatingcount=0;
+
+        HashMap<Character,Integer> hashmap = new HashMap<>();
+        for(char ch: input.toCharArray()) {
+            if (ch != ' ') {
+                hashmap.put(ch, hashmap.getOrDefault(ch, 0) + 1);
+            }
+        }
+
+        for(int frequency: hashmap.values()){
+            if(frequency>1){
+                repeatingcount++;
+            }
+        }
+        System.out.println(repeatingcount);
+
+    }
+}
