284e331eaaf111e44858a5dfad588a3bc68621fa
navaneethhhk <hknavaneeth@gmail.com>
2025-11-03 19:20:02 +0530

    3010

---
+++ src/November/p2/l6.java
@@ -0,0 +1,15 @@
+package November.p2;
+
+public class l6 {
+    public static void main(String[] args) {
+        String input="grgi45bgrpojhr54oiytepi345klmndbs45tdkjsg34toijherw34oiohgre5knger345";
+                String substring="345";
+                int count=0;
+                int index=0;
+                while((index=input.indexOf(substring,index))!=-1){
+                    count++;
+                    index=index+substring.length();
+                }
+        System.out.println(count);
+    }
+}
---
+++ src/November/p2/l7.java
@@ -0,0 +1,14 @@
+package November.p2;
+
+import java.util.Arrays;
+public class l7 {
+    public static void main(String[] args) {
+        int[] arr={43,65,78,98,6,54,3,2,32,45,56,67,78,7,90};
+        for(int i=0;i<=arr.length-1;i=i+3){
+            int temp=arr[i];
+            arr[i]=arr[i+2];
+            arr[i+2]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
