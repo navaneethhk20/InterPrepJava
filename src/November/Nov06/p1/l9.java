5d1c8cf44b08ae9036d913b8499771ffe481aea7
navaneethhhk <hknavaneeth@gmail.com>
2025-11-06 21:37:16 +0530

    3010

---
+++ src/November/Nov06/p1/l9.java
@@ -0,0 +1,16 @@
+package November.Nov06.p1;
+
+import java.util.Arrays;
+
+public class l9 {
+    public static void main(String[] args) {
+        int[] arr={23,45,665,767,89,634,23,345,546,432,3456,657,86,54,90};
+        for(int i=0;i<=arr.length-1;i=i+3){
+            int temp=arr[i];
+            arr[i]=arr[i+2];
+            arr[i+2]=temp;
+
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
