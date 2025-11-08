68c45e3415bd8fd8c9aca736024b4ca997f87f8f
navaneethhhk <hknavaneeth@gmail.com>
2025-11-08 21:10:00 +0530

    0811

--- src/November/Nov08/p1/l5.java
+++ src/November/Nov08/p1/l5.java
@@ -1,20 +1,29 @@
 package November.Nov08.p1;
-import java.util.HashMap;
+import java.util.HashSet;
 public class l5 {
     public static void main(String[] args) {
-        int[] num1 = {1, 2, 4, 4, 2, 1};
+        int[] num1 = {1, 2,4, 4, 2, 1};
         int[] num2 = {2, 2, 3};
 
-        HashMap<Integer, Integer> hashmap = new HashMap<>();
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
 
-        for (int num : num2) {
-            hashmap.put(num, 1);
-        }
+        for (int num : num1) set1.add(num);
+        for (int num : num2) set2.add(num);
+
 
         for (int num : num1) {
-            if (hashmap.containsKey(num)) {
+            //not common: if (!set2.contains(num)) {
+            if (set2.contains(num)) {
+                System.out.println(num);
+            }
+        }
+
+        for (int num : num2) {
+            if (set1.contains(num)) {
                 System.out.println(num);
             }
         }
     }
 }
+
