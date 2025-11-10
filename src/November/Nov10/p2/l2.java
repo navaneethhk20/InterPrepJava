3dbd69f765bf76a43706501194d7873fb6e7cb69
navaneethhhk <hknavaneeth@gmail.com>
2025-11-10 21:44:38 +0530

    1011

---
+++ src/November/Nov10/p2/l2.java
@@ -0,0 +1,15 @@
+package November.Nov10.p2;
+
+public class l2 {
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+        int product=1;
+        for(int num:arr){
+            product=product*num;
+        }
+
+        for(int num1:arr){
+            System.out.print(product/num1+" ");
+        }
+    }
+}
