99b821a010d831f5bc8191926f92f57165fbe20d
navaneethhhk <hknavaneeth@gmail.com>
2025-11-10 09:24:46 +0530

    0811

--- src/November/Nov09/p2/p9.java
+++ src/November/Nov09/p2/p9.java
@@ -1,8 +1,26 @@
 package November.Nov09.p2;
 
 public class p9 {
+    static int count=0;
     public static void main(String[] args) {
-
+        String input="navaneeth";
+        System.out.println("permutation is "+input+":");
+        permutation(input,"");
+        System.out.println("total count is "+count);
 
     }
+
+            public static void permutation(String str, String result) {
+                if (str.length() == 0) {
+                    count++;
+                    System.out.println(result);
+                    return;
+                }
+
+                for (int i = 0; i <= str.length() - 1; i++) {
+                    char ch = str.charAt(i);
+                    String remainning = str.substring(0, i) + str.substring(i + 1);
+                    permutation(remainning, result + ch);
+                }
+            }
 }
