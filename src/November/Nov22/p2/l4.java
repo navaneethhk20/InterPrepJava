9656eb8502b9d5a06bb821d687c8095d4fed9ec8
navaneethhhk <hknavaneeth@gmail.com>
2025-11-22 22:53:04 +0530

    2211

---
+++ src/November/Nov22/p2/l4.java
@@ -0,0 +1,14 @@
+package November.Nov22.p2;
+
+public class l4 {
+    public static void main(String[] args) {
+        String input="I Love Java Programming";
+        String[] words =input.split("\\s+");
+        String rev="";
+        for(int i=words.length-1;i>=0;i--){
+           rev= rev+words[i]+" ";
+        }
+        System.out.println(rev);
+
+    }
+}
