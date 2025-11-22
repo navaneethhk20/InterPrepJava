d84b683a1801b754effc8a1681fa4834abca14d7
navaneethhhk <hknavaneeth@gmail.com>
2025-11-22 21:39:01 +0530

    2211

--- src/November/Nov22/p1/l6.java
+++ src/November/Nov22/p1/l6.java
@@ -3,6 +3,15 @@ package November.Nov22.p1;
 public class l6 {
     public static void main(String[] args) {
 
+        String input="gersg34ghytj635hfd543hiu8534nbdsg875342fw3234ydfsfd534";
+        String substirng="34";
 
+        int count=0,index=0;
+        while((index=input.indexOf(substirng,index))!=-1){
+               count++;
+               index=substirng.length()+index;
+
+        }
+        System.out.println(count);
     }
 }
---
+++ src/November/Nov22/p1/l7.java
@@ -0,0 +1,16 @@
+package November.Nov22.p1;
+
+import java.util.Arrays;
+
+public class l7 {
+    public static void main(String[] args) {
+        int[] arr={1,2,34,43,5,6,34,7,48,8,94,9,54,52,554,5436,7,8};
+
+        for(int i=0;i<=arr.length-1;i=i+3){
+            int temp=arr[i];
+            arr[i]=arr[i+2];
+            arr[i+2]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
