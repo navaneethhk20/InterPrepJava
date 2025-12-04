fd1a809d4082169214b64a75455cc814b17b1087
navaneethhhk <hknavaneeth@gmail.com>
2025-12-04 10:34:00 +0530

    0112

---
+++ src/December/Dec04/p1/L1.java
@@ -0,0 +1,13 @@
+package December.Dec04.p1;
+
+public class L1 {
+    public static void main(String[] args){
+        String input="nithu";
+        String rev="";
+
+        for(int i=input.length()-1;i>=0;i--){
+            rev=rev+input.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
