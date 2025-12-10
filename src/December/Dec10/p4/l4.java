5c020d6d9d2db4158ab98ddec701e0c754e7c0b5
navaneethhhk <hknavaneeth@gmail.com>
2025-12-10 22:53:07 +0530

    1012

---
+++ src/December/Dec10/p4/l4.java
@@ -0,0 +1,33 @@
+package December.Dec10.p4;
+
+import java.util.Scanner;
+
+public class l4 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the character");
+//        char ch= sc.next().toLowerCase().charAt(0);
+//
+//        if(ch=='a' || ch== 'e' || ch== 'i' || ch=='o' ||ch=='u'){
+//            System.out.println("vowel");
+//        }else {
+//            System.out.println("cosnoant");
+//        }
+//        sc.close();
+
+        String input= sc.nextLine().toLowerCase();
+        int vowel=0;
+        int consonant=0;
+        for(char ch:input.toCharArray()) {
+            if (Character.isAlphabetic(ch)) {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowel++;
+                } else {
+                    consonant++;
+                }
+            }
+        }
+        System.out.println(vowel);
+        System.out.println(consonant);
+    }
+}
---
+++ src/December/Dec10/p4/l5.java
@@ -0,0 +1,14 @@
+package December.Dec10.p4;
+
+public class l5 {
+    public static void main(String[] args) {
+        int num=5;
+
+        for(int i=0;i<=num;i++){
+            for(int j=i;j<=num;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
