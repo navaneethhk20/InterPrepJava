716064322f0587c619a2876ce91ce3ee099cf6f2
navaneethhhk <hknavaneeth@gmail.com>
2025-12-12 21:02:09 +0530

    1212

---
+++ src/December/Dec12/p1/l1.java
@@ -0,0 +1,12 @@
+package December.Dec12.p1;
+
+public class l1 {
+    public static void main(String[] args) {
+        String input="trutht";
+        String rev=" ";
+        for(int i=input.length()-1;i<=0;i--){
+            rev=rev+input.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
---
+++ src/December/Dec12/p1/l2.java
@@ -0,0 +1,12 @@
+package December.Dec12.p1;
+
+public class l2 {
+    public static void main(String[] args) {
+        String input="trops";
+        String rev=" ";
+
+       StringBuilder sb= new StringBuilder(input);
+       rev=sb.reverse().toString();
+        System.out.println(rev);
+    }
+}
