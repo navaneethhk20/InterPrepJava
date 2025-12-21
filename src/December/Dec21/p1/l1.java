ae66da576344863cf807993f5431c86f13bc0c4d
navaneethhhk <hknavaneeth@gmail.com>
2025-12-21 10:35:57 +0530

    2012

---
+++ src/December/Dec21/p1/l1.java
@@ -0,0 +1,11 @@
+package December.Dec21.p1;
+
+public class l1 {
+    public static void main(String[] args){
+        String input="navaneeth";
+        StringBuilder sb = new StringBuilder(input);
+        String rev= sb.reverse().toString();
+        System.out.println(rev);
+
+    }
+}
