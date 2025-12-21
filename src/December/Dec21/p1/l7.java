f2eae7d7e72f5b0bf062cd52d24f2fa00724680e
navaneethhhk <hknavaneeth@gmail.com>
2025-12-21 11:14:51 +0530

    2012

---
+++ src/December/Dec21/p1/l7.java
@@ -0,0 +1,15 @@
+package December.Dec21.p1;
+
+import java.util.Arrays;
+
+public class l7 {
+    public static void main(String[] args) {
+        int []arr={1,2,3,4,5,6,7,8,7,47,66,5,44,3,2};
+        for(int i=0;i<=arr.length-1;i=i+3){
+            int temp=arr[i];
+            arr[i]=arr[i+2];
+            arr[i+2]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
