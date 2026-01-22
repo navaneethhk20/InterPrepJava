b73373b87c48431a8b68ea04ca20cea6b68d852d
navaneethhhk <hknavaneeth@gmail.com>
2026-01-22 08:47:27 +0530

    2101

--- src/Year2026/Jan/Day21/l1.java
+++ src/Year2026/Jan/Day21/l1.java
@@ -3,7 +3,9 @@ package Year2026.Jan.Day21;
 public class l1 {
     public static void main(String[] args) {
         String input="automation";
-
+       StringBuilder sb= new StringBuilder(input);
+       String rev= sb.reverse().toString();
+       System.out.println(rev);
 
 
     }
---
+++ src/Year2026/Jan/Day21/l2.java
@@ -0,0 +1,7 @@
+package Year2026.Jan.Day21;
+
+public class l2 {
+    public static void main(String[] args) {
+
+    }
+}
